59dc0819abbc5622b6ac71e3776b46e93e859526
Garret Gallant <gallanttechnological@gmail.com>
2022-08-30 10:01:07 -0600

    commit tree b1e4772f3b8699e8b3a2ffbbfd9eab3a8badb0b4

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,20 @@
+class StudentsController < ApplicationController
+
+  def index 
+    students = Student.all
+    render json: students
+  end
+
+  def grades
+    students = Student.order('grade DESC')
+    render json: students
+  end
+
+  def highest_grade
+    students = Student.order('grade DESC')
+    render json: students.first
+  end
+
+  
+
+end
